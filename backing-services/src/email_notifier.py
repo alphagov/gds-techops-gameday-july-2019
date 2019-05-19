820570cd697c441454a5830be60fcb4ffbf94fe4
OllieJC <oliver.chalk@digital.cabinet-office.gov.uk>
2019-05-19 13:55:35 +0100

    New Makefile, basic tests with tox
    - new Makefile which cleans, builds, runs
      - `make clean` will clear up all zip, tox and build artefacts
      - `make zip` will make a zip to be deployed to Lambda
      - `make run` will copy files, download dependencies and run `game_play.py`
    - basic test file `test_game_play.py` created
      - need to work out how to do auth tests
      - _game_play should move to form based auth instead of digest_
    - documentation generator from markdown files
      - so `/docs/<path>` will take md files from `src/game_play_docs` and generate HTML

--- backing-services/src/email_notifier.py
+++ backing-services/src/email_notifier.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
-
-"""email-notifier.py: This Lambda will take requests to be 'emailed'; however,
-this will actually go into the scoring system where quantity of completed requests."""
-
-__license__ = "MIT"
-__status__ = "Prototype"
-
 import os
 import re
 import random
 import string
 import json
-from flask import (
-    Flask,
-    request,
-)
+from flask import Flask, request
 from flask_httpauth import HTTPTokenAuth
 
 app = Flask(__name__)
-auth = HTTPTokenAuth('Bearer')
+auth = HTTPTokenAuth("Bearer")
 
 DEFAULT_OK_RESPONSE = "OK"
+
 
 @auth.verify_token
 def verify_token(token):
@@ -36,29 +27,29 @@ def verify_token(token):
 
 @app.route("/")
 def root():
-    ua = request.headers.get('User-Agent')
+    ua = request.headers.get("User-Agent")
 
-    if 'ELB-HealthChecker' in ua:
+    if "ELB-HealthChecker" in ua:
         print("This is a Health Check Request")
         return "GTG"
 
     return DEFAULT_OK_RESPONSE
 
 
-@app.route("/api", methods = ['GET', 'POST'])
+@app.route("/api", methods=["GET", "POST"])
 @auth.login_required
 def api():
     return DEFAULT_OK_RESPONSE
 
 
-@app.route("/api/v1", methods = ['GET', 'POST'])
+@app.route("/api/v1", methods=["GET", "POST"])
 @auth.login_required
 def api_v1():
     # where the Ruby app will initially be configured to send requests
     return DEFAULT_OK_RESPONSE
 
 
-@app.route("/api/v2", methods = ['GET', 'POST'])
+@app.route("/api/v2", methods=["GET", "POST"])
 @auth.login_required
 def api_v2():
     # 'new' api that needs to be made live before v1 is deprecated
@@ -67,6 +58,5 @@ def api_v2():
 
 if __name__ == "__main__":
     app.config["ENV"] = "development"
-    app.config["TESTING"] = True
     app.config["DEBUG"] = True
     app.run(port=5000)
---
+++ backing-services/src/game_play_docs/default.md
@@ -0,0 +1,4 @@
+## default
+
+### test
+[test](/docs/test)  
