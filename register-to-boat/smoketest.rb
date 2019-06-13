00bc57424bb3f78ea5ae9b928bd71cd119416ab4
Tristram Oaten <tristram@oaten.name>
2019-06-13 16:55:12 +0100

    smoke test uuid

--- register-to-boat/smoketest.rb
+++ register-to-boat/smoketest.rb
@@ -1,8 +1,12 @@
-`curl -s 'http://localhost:4567/register' --data 'first_name=Troll&last_name=Face'`
+require 'securerandom'
+
+id = SecureRandom.uuid
+
+`curl -s 'http://localhost:4567/register' --data 'first_name=Troll&last_name=Face#{id}'`
 splunk_messages = `curl -s -k -u admin:correcthorsebatterystaple https://localhost:8089/services/messages`
 
-if splunk_messages.include?('Troll')
-  puts '[PASS] Troll user successfully logged'
+if splunk_messages.include?(id)
+  puts "[PASS] Troll user successfully logged with id " + id
 else
-  puts '[FAIL] No logging'
+  puts "[FAIL] No logging, can't find Troll user with id " + id
 end
