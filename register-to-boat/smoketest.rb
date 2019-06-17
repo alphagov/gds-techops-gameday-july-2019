10d3bec45d3ce4002fd4617095b8a7bfa188d7e1
Tristram Oaten <tristram@oaten.name>
2019-06-17 16:59:52 +0100

    Switched from splunk messages to splunk events
    Paramaterised some envs into web.env

--- register-to-boat/smoketest.rb
+++ register-to-boat/smoketest.rb
@@ -1,12 +1,22 @@
 require 'securerandom'
+require 'json'
 
 id = SecureRandom.uuid
 
 `curl -s 'http://localhost:4567/register' --data 'first_name=Troll&last_name=Face#{id}'`
-splunk_messages = `curl -s -k -u admin:correcthorsebatterystaple https://localhost:8089/services/messages`
 
-if splunk_messages.include?(id)
+search_response = `curl -s -k -u 'admin:correcthorsebatterystaple' https://localhost:8089/services/search/jobs  -d search="search source=\"testlog\"" -d output_mode=json`
+
+search_id = JSON.parse(search_response)["sid"]
+
+puts "Waiting for the search to complete..."
+sleep 1
+puts "...OK"
+
+splunk_messages = `curl -s -k -u 'admin:correcthorsebatterystaple' https://localhost:8089/services/search/jobs/#{search_id}/results/ --get -d output_mode=json`
+
+if splunk_messages.include? id
   puts "[PASS] Troll user successfully logged with id " + id
 else
-  puts "[FAIL] No logging, can't find Troll user with id " + id
+  puts "[FAIL] Can't find Troll user registered in splunk with id " + id
 end
