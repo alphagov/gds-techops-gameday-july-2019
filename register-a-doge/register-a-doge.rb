314b0907b62999db25f568648ee0b918a775038f
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-06-20 23:04:20 +0100

    remove splunk from app, participants have to impl
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- register-a-doge/register-a-doge.rb
+++ register-a-doge/register-a-doge.rb
@@ -22,36 +22,7 @@ get '/register' do
   }
 end
 
-def splunk_message(logger, message)
-  uri = URI.parse("#{ENV['SPLUNK_URI']}/services/collector/event")
-  begin
-    request = Net::HTTP::Post.new(uri)
-    request.basic_auth('x', ENV['SPLUNK_KEY'])
-    request.body = { event: message, sourcetype: 'userAlert' }.to_json
-
-    response = Net::HTTP.start(
-      uri.hostname, uri.port,
-      use_ssl: uri.scheme == 'https', verify_mode: OpenSSL::SSL::VERIFY_NONE
-    ) { |http| http.request(request) }
-
-    logger.info response.body
-
-    if response.code.to_s.match?(/^2+/)
-      logger.info 'Success posting to splunk'
-    else
-      logger.error "Failure posting to splunk: #{response.body}"
-    end
-  rescue StandardError => e
-    logger.error e
-  end
-end
-
 post '/register' do
-  splunk_message(
-    logger,
-    "POST /register: [fname=#{params[:first_name]} ; lname=#{params[:last_name]}]"
-  )
-
   registration = Registration.new
   registration.first_name = params[:first_name]
   registration.last_name = params[:last_name]
