5bd3206b0162d4f3728dfbb81d63b40acdc777d4
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-06-21 22:25:18 +0100

    Auto map subnet on launch
    nothing works if this isnt a thing
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- terraform/modules/app-deployment/vpc.tf
+++ terraform/modules/app-deployment/vpc.tf
@@ -34,6 +34,7 @@ resource "aws_subnet" "z1" {
   vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_route_table_association" "z1" {
@@ -49,6 +50,7 @@ resource "aws_subnet" "z2" {
   vpc_id            = "${aws_vpc.main.id}"
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-west-2b"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_route_table_association" "z2" {
